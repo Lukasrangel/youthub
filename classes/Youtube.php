6dfe6a4578a4b20ab06805e6b03c06cfc0f93045
Lukas Dev <103273579+Lukasrangel@users.noreply.github.com>
2025-08-10 15:27:19 -0300

    Update Youtube.php

--- classes/Youtube.php
+++ classes/Youtube.php
@@ -9,51 +9,47 @@ class Youtube {
         $url = explode('&',$link);
         return $url[0];
     }
-    
+
 
     public function mp3($link) {
 
         $link = $this->explode($link);
-        
+
         try {
-            $command = "python bin/mp3.py " . $link; 
+            $command = "yt-dlp -o 'mp3/%(title)s.%(ext)s' -x --audio-format mp3 " . $link;
             $output = shell_exec($command);
         } catch(Exception $e) {
             echo $e->getMessage();
         }
-        
+
         $resultado['format'] = 'mp3';
-        preg_match('/FixupM4a(.*)m4a/', $output, $output_array);
-        $file = explode('//',$output_array[0])[1];
+        preg_match('/\[ExtractAudio\] Destination:\s*mp3\/(.*?)\.mp3/', $output, $output_array);
+        $file = $output_array[1] . '.mp3';
 
         $resultado['file'] = $file;
-        
+
         echo json_encode($resultado);
-        
+
     }
 
     public function mp4($link) {
-        
+
         $link = $this->explode($link);
-        
+
         try {
-            $command = "python bin/mp4.py " . $link; 
+            $command = "yt-dlp -o 'mp4/%(title)s.%(ext)s' " . $link;
             $output = shell_exec($command);
         } catch(Exception $e) {
             echo $e->getMessage();
         }
-        
+
         $resultado['format'] = 'mp4';
-        preg_match('/Merger(.*)"mp4\/\/(.*)"/', $output, $output_array);
-        
+        preg_match('/\[Merger\] Merging formats into "mp4\/(.*?)\.mp4"/', $output, $output_array);
+
         $resultado['file'] = $output_array[2];
 
         echo json_encode($resultado);
-
-
     }
-
-
 }
 
 ?>
